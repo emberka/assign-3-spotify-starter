c836be2df5260b1c140d02e450593f10426991cd
ember fu <71354928+emberka@users.noreply.github.com>
2022-02-11 22:50:42 -0800

    re-pushing assign3 code

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const CLIENT_ID = "7462f178922d4f1fadcf933a86eeb077";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private"
+];
+export const REDIRECT_URI = "exp://10.129.19.36:19000"; 
+export const ALBUM_ID = "6jHsnYqSmiMqBvzG8qrYuE?si=xZ2M0fIfR5uExV-cvIwcvw";
